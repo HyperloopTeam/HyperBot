a6b7f585c8461d0fcbb6c49254db28853b542fee
Shreyas <shreyashirday@gmail.com>
2015-11-03 19:38:44 -0500

    added deadlines functionality

--- hyperbot.js
+++ hyperbot.js
@@ -1,7 +1,55 @@
 module.exports = function(req, res, next) {
   var userName = req.body.user_name;
+
+  var triggerWord = req.body.trigger_word;
+  var txt = req.body.text;
+
+  var response = '';
+
+  if(triggerWord === 'ask:'){
+
+      var tl = triggerWord.length;
+      var query = txt.substring(tl);
+      var podSpecs = ~query.indexOf("specs") && ~query.indexOf("pod");
+      var tubeSpecs = ~query.indexOf("specs") && ~query.indexOf("tube");
+
+      if(podSpecs){
+          response = 'Pod: Mass: 500kg';
+      }
+      if(tubeSpecs){
+        if(response.length > 0){
+          response += ', Tube: Material: ASTM A1018 Grade'
+        }
+        else{
+          response = 'Tube: insert tube specs'
+        }
+      }
+
+
+  }
+  else if(triggerWord === 'deadlines:'){
+    var tl = triggerWord.length;
+    var query = txt.substring(tl);
+
+    if(query === 'team'){
+
+    }
+    else if(query === 'spacex'){
+        response = 'Prelim design: November 13, Final Design: January 13, Design Weekend: January 29 and 30';
+    }
+    else{
+      response = ''
+    }
+  }
+  else if(triggerWord === 'search:'){
+      response = 'Under Construction';
+  }
+  else{
+    response = 'Yo, ' + userName + '! Type \'deadlines:<optional - team or spacex>\', \'ask:<any text containing \'specs\' and either \'pod\' or \'tube\'>\',\'search:<whatever you want to search in Google Drive>\'';
+  }
+
   var botPayload = {
-    text: 'Yo, ' + userName + '!'
+    text: response;
   };
 
 if(userName !== 'slackbot'){
