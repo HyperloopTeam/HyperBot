dcaa6eda3642291402667767b673ac15ca77fad6
Shreyas <shreyashirday@gmail.com>
2015-11-11 18:20:34 -0500

    fix?

--- hyperbot.js
+++ hyperbot.js
@@ -72,7 +72,8 @@ module.exports = function(req, res, next) {
       if(first_letter === 'A'){
         var announcement = query.substring(1);
         var a = {author : userName, date: new Date(), message: announcement};
-        db.announcements.insert(a, function(err, result){
+        var collection = db.collection('announcements');
+        collection.insert(a, function(err, result){
           if(err) {
             console.log(err);
             response = 'error: ' + err;
@@ -89,7 +90,8 @@ module.exports = function(req, res, next) {
       else if(first_letter === 'D'){
         var deadline = query.substring(1);
         var d = {author: userName, date : new Date(), message: deadline};
-        db.deadlines.insert(d, function(err, result){
+        var collection = db.collection('deadlines');
+        collection.insert(d, function(err, result){
           if(err){
             console.log(err);
             response = 'error: ' + err;
